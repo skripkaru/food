afe1f54e3db3d182f81f4ca07699a1a533c446bf
Evgeniy Skripkaru <skripkaru.evgeniy@gmail.com>
2021-05-17 18:55:48 +0300

    Реализация модального окна

--- src/js/main.js
+++ src/js/main.js
@@ -42,59 +42,108 @@ window.addEventListener('DOMContentLoaded', function () {
 	});
 
 
-		// Timer
+	// Timer
 
-		const deadline = '2022-01-01';
+	const deadline = '2022-01-01';
 
-		function getTimeRemaining(endtime) {
-			const t = Date.parse(endtime) - Date.parse(new Date()),
-				days = Math.floor((t / (1000 * 60 * 60 * 24))),
-				seconds = Math.floor((t / 1000) % 60),
-				minutes = Math.floor((t / 1000 / 60) % 60),
-				hours = Math.floor((t / (1000 * 60 * 60) % 24));
-	
-			return {
-				'total': t,
-				'days': days,
-				'hours': hours,
-				'minutes': minutes,
-				'seconds': seconds
-			};
+	function getTimeRemaining(endtime) {
+		const t = Date.parse(endtime) - Date.parse(new Date()),
+			days = Math.floor((t / (1000 * 60 * 60 * 24))),
+			seconds = Math.floor((t / 1000) % 60),
+			minutes = Math.floor((t / 1000 / 60) % 60),
+			hours = Math.floor((t / (1000 * 60 * 60) % 24));
+
+		return {
+			'total': t,
+			'days': days,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds
+		};
+	}
+
+	function getZero(num) {
+		if (num >= 0 && num < 10) {
+			return '0' + num;
+		} else {
+			return num;
 		}
-	
-		function getZero(num) {
-			if (num >= 0 && num < 10) {
-				return '0' + num;
-			} else {
-				return num;
+	}
+
+	function setClock(selector, endtime) {
+
+		const timer = document.querySelector(selector),
+			days = timer.querySelector("#days"),
+			hours = timer.querySelector('#hours'),
+			minutes = timer.querySelector('#minutes'),
+			seconds = timer.querySelector('#seconds'),
+			timeInterval = setInterval(updateClock, 1000);
+
+		updateClock();
+
+		function updateClock() {
+			const t = getTimeRemaining(endtime);
+
+			days.innerHTML = getZero(t.days);
+			hours.innerHTML = getZero(t.hours);
+			minutes.innerHTML = getZero(t.minutes);
+			seconds.innerHTML = getZero(t.seconds);
+
+			if (t.total <= 0) {
+				clearInterval(timeInterval);
 			}
 		}
-	
-		function setClock(selector, endtime) {
-	
-			const timer = document.querySelector(selector),
-				days = timer.querySelector("#days"),
-				hours = timer.querySelector('#hours'),
-				minutes = timer.querySelector('#minutes'),
-				seconds = timer.querySelector('#seconds'),
-				timeInterval = setInterval(updateClock, 1000);
-	
-			updateClock();
-	
-			function updateClock() {
-				const t = getTimeRemaining(endtime);
-	
-				days.innerHTML = getZero(t.days);
-				hours.innerHTML = getZero(t.hours);
-				minutes.innerHTML = getZero(t.minutes);
-				seconds.innerHTML = getZero(t.seconds);
-	
-				if (t.total <= 0) {
-					clearInterval(timeInterval);
-				}
-			}
+	}
+
+	setClock('.timer', deadline);
+
+	// Modal
+
+	const modalTrigger = document.querySelectorAll('[data-modal]'),
+		modal = document.querySelector('.modal'),
+		modalCloseBtn = document.querySelector('[data-close]');
+
+	function openModal() {
+		modal.classList.add('show', 'fade');
+		modal.classList.remove('hide');
+		document.body.style.overflow = 'hidden';
+		clearInterval(modalTimerId);
+	}
+
+	modalTrigger.forEach(btn => {
+		btn.addEventListener('click', openModal);
+	});
+
+	function closeModal() {
+		modal.classList.add('hide');
+		modal.classList.remove('show', 'fade');
+		document.body.style.overflow = '';
+	}
+
+	modalCloseBtn.addEventListener('click', closeModal);
+
+	modal.addEventListener('click', (e) => {
+		if (e.target === modal) {
+			closeModal();
 		}
-	
-		setClock('.timer', deadline);
+	});
+
+	document.addEventListener('keydown', (e) => {
+		if (e.code === "Escape" && modal.classList.contains('show')) {
+			closeModal();
+		}
+	});
+
+	// const modalTimerId = setTimeout(openModal, 5000);
+
+
+	function showModalByScroll() {
+		if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+			openModal();
+			window.removeEventListener('scroll', showModalByScroll);
+		}
+	}
+
+	window.addEventListener('scroll', showModalByScroll);
 
 });
